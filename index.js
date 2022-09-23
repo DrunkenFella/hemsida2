066ce8bc42c60a7e5d854c1d066e421d3e3e3836
Drunken Fella <alex.ostlund@elev.ga.ntig.se>
2022-09-23 21:20:40 +0200

    pp

---
+++ index.js
@@ -0,0 +1,23 @@
+const JSONdb = require('simple-json-db');
+
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const http = require('http')
+
+//const subdomain = require('express-subdomain');
+const admin = require(path.join(__dirname, 'admin/admin'));
+
+
+app.use('/admin', admin);
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, '/assets/images/favicon.ico'))
+});
+
+
+http.createServer(app).listen(5500);
